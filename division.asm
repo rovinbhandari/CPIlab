bd01861f49afac71639df29b9b1b09ebaee40377
Rajat Khanduja <rajatkhanduja13@gmail.com>
2012-08-02 11:42:54 +0530

    Division now functional

--- division.asm
+++ division.asm
@@ -1,32 +1,22 @@
-  LXI H,C050
-  MOV C,M
-  LXI H,C051
-  MOV B,M
-  LXI H,C052
-  MOV E,M
-  LXI H,C053
-  MOV D,M
-  MVI L,00
-  MVI H,00
+  LHLD C050   # Load from memory location C050 to HL pair.
+  XCHG        # Transfer from HL pair to DE pair
+  LHLD C052   # Load from memory location C052 to HL pair.
+  LXI B,0000H # Set BC to 0
 loop:
-  MOV A,B
-  CMP D
-  JC EXIT
-  JNC check
-subt:
-  MOV A,C
-  SUB E
-  MOV C,A
-  MOV A,B
-  SBB D
-  MOV B,A
-  INX H
-  JMP loop
-check:
-  MOV A,C
-  CMP E
-  JC exit
-  JMP subt
-exit:
-  SHLD C054
+  MOV A,L     # A <- L [copy the lower 8 bits ]
+  SUB E       # A = A - E [subtract the lower 8 bits ]
+  MOV L,A     # L <- A
+  MOV A,H     # A <- H [copy the higher 8 bits]
+  SBB D       # Subtract the higher 8 bits with borrow.
+  MOV H,A     # H <- A
+  INX B       # Increment B 
+  JNC loop    # If not carry (which occurs if the subtraction yielded a negative number) Jump to loop
+
+  DCX B       # Since we over-counted B, decrement B
+  DAD D       # Add DE to HL (makes it positive)
+  SHLD C054   # Puts remainder at C054  
+  MOV A,C     # A <- C
+  STA C056    # Store lower 8 bits to memory location
+  MOV A,B     # A <- B
+  STA C057    # Store higher 8 bits to memory location
   HLT
